d0351130612dbb713d7a683b3f9dd86f169ac955
emdeh <72545829+emdeh@users.noreply.github.com>
2024-08-31 18:55:15 +1000

    docstrings with requirements added

--- train.py
+++ train.py
@@ -1,4 +1,23 @@
 '''
 This file is to train a new network on a dataset and save the model as a 
 checkpoint.
+
+Requirements:
+Train a new network on a data set with train.py
+
+Basic usage: python train.py data_directory
+
+- Prints out training loss, validation loss, and validation accuracy as the network trains
+- Options:
+    Set directory to save checkpoints
+        python train.py data_dir --save_dir save_directory
+
+    Choose architecture
+        python train.py data_dir --arch "vgg13"
+
+    Set hyperparameters
+        python train.py data_dir --learning_rate 0.01 --hidden_units 512 --epochs 20
+
+    Use GPU for training
+        python train.py data_dir --gpu
 '''
--- utils.py
+++ utils.py
@@ -1,3 +1,21 @@
 '''
 This file is for utility functions used to load data and pre-process images.
+
+Predict flower name from an image with predict.py along with the probability 
+of that name. That is, you'll pass in a single image /path/to/image and return 
+the flower name and class probability.
+
+Basic usage: python predict.py /path/to/image checkpoint
+Options:
+    Return top K most likely classes: 
+        python predict.py input checkpoint --top_k 3
+
+    Use a mapping of categories to real names:
+        python predict.py input checkpoint --category_names cat_to_name.json
+
+    Use GPU for inference:
+        python predict.py input checkpoint --gpu
+
+The best way to get the command line input into the scripts is with the argparse module(opens in a new tab) in the standard library. You can also find a nice tutorial for argparse here(opens in a new tab).
+
 '''
