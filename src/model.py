fe74a529758c1d0221c0ae5f858da66f3e932f06
emdeh <72545829+emdeh@users.noreply.github.com>
2024-09-05 17:51:51 +1000

    updates to model

--- src/model.py
+++ src/model.py
@@ -33,7 +33,7 @@ class ModelManager:
     def _create_model(self, arch, hidden_units):
         # Create model based on architecture (used during training)
         if arch == 'vgg16':
-            model = models.vgg16(weights='VGG16_Weights')
+            model = models.vgg16(pretrained=True)
         else:
             raise ValueError(f"Unsupported architecture: {arch}")
 
