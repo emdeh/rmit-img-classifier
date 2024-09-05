9390932e822274d0cd018bd44383ea7783548c61
emdeh <72545829+emdeh@users.noreply.github.com>
2024-09-05 20:55:26 +1000

    removed unused top_p variable from line 149 in train()

--- src/model.py
+++ src/model.py
@@ -146,7 +146,7 @@ class ModelManager:
 
                             # Calculate accuracy
                             ps = torch.exp(outputs)
-                            top_p, top_class = ps.topk(1, dim=1)
+                            top_class = ps.topk(1, dim=1)
                             equals = top_class == labels.view(*top_class.shape)
                             accuracy += torch.mean(equals.type(torch.FloatTensor)).item()
                             #print("Debug: Calculated accuracy")
@@ -204,7 +204,7 @@ class ModelManager:
             map_location = 'cpu'
 
         print(f"Loading checkpoint from: {checkpoint_path}")
-        
+
         warnings.simplefilter("ignore")
         # TODO: Implement logging
 
